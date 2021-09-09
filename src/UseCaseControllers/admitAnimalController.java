bc489310fc9112c72c8ee0e129af58e87b99c850
Chad-Conning <chad.conning@gmail.com>
2021-09-09 15:54:18 +0200

    added stuff for admit animal

--- src/UseCaseControllers/admitAnimalController.java
+++ src/UseCaseControllers/admitAnimalController.java
@@ -1,4 +1,25 @@
 package UseCaseControllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import sample.Database;
+
+import java.sql.Connection;
+
 public class admitAnimalController {
+    Database queries = new Database();
+    Connection connection;
+    Scene scene;
+    String staffRole;
+
+    @FXML private Button btnAdmit;
+    @FXML private Button btnACancel;
+    @FXML private TextField tfieldTag;
+    @FXML private DatePicker datePicker;
+    @FXML private ComboBox comboLocation;
+    @FXML private TextArea txtNotes;
+
+
+
 }
