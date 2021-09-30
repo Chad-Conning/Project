eeb86879ae55708a7bf87aa91a341e7e706e3c25
Patrick <patrickbamber@gmail.com>
2021-09-30 18:59:30 +0200

    Update modifyStaffController.java
    working on the modify staff - still need to make changes

--- src/UseCaseControllers/modifyStaffController.java
+++ src/UseCaseControllers/modifyStaffController.java
@@ -13,7 +13,9 @@ import sample.Staff;
 
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -40,7 +42,7 @@ public class modifyStaffController {
 
     public void initSessionID(Scene scene, Staff staffUser) {
         comboMSStaffType.getItems().addAll("Administrator", "Admission", "Handler");
-        comboMSStaffType.getSelectionModel().select("Admission");
+        comboMSStaffType.getSelectionModel().select("Administrator");
         this.staffUser = staffUser;
         this.scene = scene;
         try {
@@ -54,38 +56,87 @@ public class modifyStaffController {
         btnMSRegister.setOnAction(actionEvent -> {
             Boolean result = validationControl();
             if (result)
-                addStaff();
+                modifyStaff();
         });
 
         btnMSCancel.setOnAction(actionEvent -> showMainView());
     }
 
-    private void addStaff() {
-        //on action register
+//    private void modifyStaff() {
+//        //on action register
+//
+//        connection = queries.connection;
+//        String txtPassword = tfieldPassword.getText();
+//        String txtfName = tfieldMSName.getText();
+//        String txtlName = tfieldMSSurname.getText();
+//        String txtContact = tfieldMSPhone.getText();
+//        String txtEmail = tfieldMSEmail.getText();
+//        String txtTaxNum = tfieldMSTax.getText();
+//        String txtStaffType = comboMSStaffType.getValue().toString();
+//
+//        if (queries.addStaff(txtPassword, txtfName, txtlName, txtContact, txtEmail, txtTaxNum, txtStaffType)) {
+//            Alert added = new Alert(Alert.AlertType.INFORMATION, "The new staff member has been added.");
+//            added.showAndWait();
+//        } else {
+//            Alert added = new Alert(Alert.AlertType.INFORMATION, "The new staff member could not be added.");
+//            added.showAndWait();
+//        }
+//        try {
+//            queries.connection.close();
+//        } catch (SQLException throwables) {
+//            throwables.printStackTrace();
+//        }
+//        //showMainView();
+//    }
 
-        connection = queries.connection;
-        String txtPassword = tfieldPassword.getText();
-        String txtfName = tfieldMSName.getText();
-        String txtlName = tfieldMSSurname.getText();
-        String txtContact = tfieldMSPhone.getText();
-        String txtEmail = tfieldMSEmail.getText();
-        String txtTaxNum = tfieldMSTax.getText();
-        String txtStaffType = comboMSStaffType.getValue().toString();
+    private void modifyStaff() {
 
-        if (queries.addStaff(txtPassword, txtfName, txtlName, txtContact, txtEmail, txtTaxNum, txtStaffType)) {
-            Alert added = new Alert(Alert.AlertType.INFORMATION, "The new staff member has been added.");
-            added.showAndWait();
-        } else {
-            Alert added = new Alert(Alert.AlertType.INFORMATION, "The new staff member could not be added.");
-            added.showAndWait();
+        try
+        {
+            // create our mysql database connection
+//            String myDriver = "org.gjt.mm.mysql.Driver";
+//            String myUrl = "jdbc:mysql://localhost/test";
+//            Class.forName(myDriver);
+//            Connection conn = DriverManager.getConnection(myUrl, "root", "");
+            connection = queries.connection;
+
+            // our SQL SELECT query.
+            // if you only need a few columns, specify them by name instead of using "*"
+            String query = "SELECT * FROM Staff";
+
+            // create the java statement
+            //Statement st = conn.createStatement();
+            Statement st = connection.createStatement();
+
+            // execute the query, and get a java resultset
+            ResultSet rs = st.executeQuery(query);
+
+            // iterate through the java resultset
+            while (rs.next())
+            {
+                int id = rs.getInt("Staff_ID");
+                //String Password = rs.getString("Password");
+                String firstName = rs.getString("Staff_FName");
+                String lastName = rs.getString("Staff_LName");
+                String contact = rs.getString("Staff_ContactNum");
+                String email = rs.getString("Staff_Email");
+                String staffType = rs.getString("Staff_Type");
+                boolean isEmployed = rs.getBoolean("is_Employed");
+
+
+                // print the results
+                System.out.format("%s, %s, %s, %s, %s, %s\n", id, firstName, lastName, contact, email, staffType, isEmployed);
+            }
+            st.close();
         }
-        try {
-            queries.connection.close();
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
+        catch (Exception e)
+        {
+            System.err.println("Got an exception! ");
+            System.err.println(e.getMessage());
         }
-        //showMainView();
     }
+    
+
 
     private void showMainView() {
         try {
