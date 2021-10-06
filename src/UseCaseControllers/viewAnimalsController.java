8568f82ff32fed82bd2fdd03a7d68a33e9aa06b5
Chad-Conning <chad.conning@gmail.com>
2021-10-06 21:05:49 +0200

    Added menu functionality to all use cases

--- src/UseCaseControllers/viewAnimalsController.java
+++ src/UseCaseControllers/viewAnimalsController.java
@@ -1,10 +1,26 @@
 package UseCaseControllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
+import sample.Database;
+import sample.LoginManager;
+import sample.Staff;
+import sample.menuController;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class viewAnimalsController {
+    Database queries = new Database();
+    Connection connection;
 
+    @FXML private Label lblUserInformation;
     @FXML public MenuItem btnMenuAddRegisterA;
     @FXML public MenuItem btnMenuAddAddS;
     @FXML public MenuItem btnMenuAddUpdateL;
@@ -15,4 +31,42 @@ public class viewAnimalsController {
     @FXML public MenuItem btnMenuDisplayAR;
     @FXML public MenuItem btnMenuDisplayLogsA;
     @FXML public MenuItem btnMenuDisplayS;
+
+    Staff staffUser;
+    Scene scene;
+
+    public void initSessionID(Scene scene, Staff staffUser) {
+        this.staffUser = staffUser;
+
+        lblUserInformation.setText("Logged in Staff ID: " + staffUser.getStaffID() + ", " + staffUser.getfName() + " " + staffUser.getlName());
+
+        this.scene = scene;
+
+        menuController menu = new menuController(scene, staffUser, btnMenuAddRegisterA, btnMenuAddAddS, btnMenuAddUpdateL, btnMenuEditModA, btnMenuEditModS,
+                btnMenuDisplayAdmis, btnMenuDisplayLog, btnMenuDisplayAR, btnMenuDisplayLogsA, btnMenuDisplayS);
+
+        try {
+            queries.connectDB();
+
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private void showMainView() {
+        try {
+            FXMLLoader loader = new FXMLLoader(
+                    getClass().getResource("/LandingPage.fxml")   // load fxml
+            );
+            scene.setRoot(loader.load());   // create scene for mainView screen
+            MainViewController controller =
+                    loader.getController();   // gets the controller specified in the fxml
+
+            LoginManager loginManager = new LoginManager(scene);
+            controller.initSessionID(loginManager, this.scene, staffUser);
+        } catch (IOException ex) {
+            Logger.getLogger(LoginManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
