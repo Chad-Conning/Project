af862bebc25a70978b3734dff297b8801d8e9db5
Patrick <patrickbamber@gmail.com>
2021-10-12 09:56:05 +0200

    added logo

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,6 +2,7 @@ package sample;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -11,11 +12,15 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Scene scene = new Scene(new StackPane());
 
+        //stylesheet
+        //scene.getStylesheets().add("bootstrap.css");
+
         // initializes loginManager to manage logins and to display different screens
         LoginManager loginManager = new LoginManager(scene);
         loginManager.showLoginScreen();
 
         primaryStage.setTitle("Bay Marine Rescue");
+        primaryStage.getIcons().add(new Image("file:Resources/Images/Images/penguin (2).png"));
         primaryStage.setScene(scene);
         primaryStage.show();
     }
